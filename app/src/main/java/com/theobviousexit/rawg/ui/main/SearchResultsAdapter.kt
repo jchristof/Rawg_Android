db7876bc41b7234928f0af26759362c51efbf5dd
Jon Martin <jonmartin@overstock.com>
2019-09-21 09:59:50 -0600

    Removing unused library

--- app/src/main/java/com/theobviousexit/rawg/ui/main/SearchResultsAdapter.kt
+++ app/src/main/java/com/theobviousexit/rawg/ui/main/SearchResultsAdapter.kt
@@ -7,8 +7,6 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.synnapps.carouselview.CarouselView
-import com.synnapps.carouselview.ImageListener
 import com.theobviousexit.rawg.R
 import com.theobviousexit.rawg.RawgResponse
 
@@ -30,17 +28,6 @@ class SearchResultsAdapter(private val searchResponse: RawgResponse): RecyclerVi
         Glide.with(holder.layout).load(searchResults.backgroundImage).into(imageView)
         val text = holder.layout.findViewById<TextView>(R.id.game_name)
         text.text = searchResults.name
-
-//        val carouselView = holder.layout.findViewById<CarouselView>(R.id.carouselView);
-//        carouselView.setPageCount(searchResults.shortScreenshots.size)
-//
-//        carouselView.setImageListener(object: ImageListener {
-//            override fun setImageForPosition(position: Int, iv: ImageView?) {
-//                val imageView = iv ?: return
-//                Glide.with(holder.layout).load(searchResults.shortScreenshots[position].image).override(600, 200).into(imageView)
-//            }
-//
-//        });
     }
 }
 
